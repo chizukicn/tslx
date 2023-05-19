8068f90b6d158223c763baae3c6626025f2ab2f0
chizukicn <chizukicn@outlook.com>
2023-05-19 10:32:12 +0800

    feat: add classnames function.

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+
+## v0.0.1
+
+
+### 🚀 Enhancements
+
+  - Add classnames function. ([6bc84e8](https://github.com/unjs/packageName/commit/6bc84e8))
+
+### ❤️  Contributors
+
+- Chizukicn <chizukicn@outlook.com>
+
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,22 @@
-import { expect, it, describe } from "vitest";
-import {} from "../src";
+import { describe, expect, it } from "vitest";
+import { cls, unit_f } from "../src";
 
-describe("packageName", () => {
-  it.todo("pass", () => {
-    expect(true).toBe(true);
+describe("tslx", () => {
+  it("test classnames", () => {
+    expect(cls({
+      foo: true,
+      bar: false
+    }, "baz", ["p-4 foo  m-3", {
+      absolute: true,
+      relative: false
+    }]).join(" ")).toBe("foo baz p-4 m-3 absolute");
+  });
+
+  it("test unit_f", () => {
+    expect(unit_f(1)).toBe("1px");
+    expect(unit_f("1")).toBe("1px");
+    expect(unit_f("1rem")).toBe("1rem");
+    expect(unit_f("1px", "rem")).toBe("1px");
+    expect(unit_f("1rem", "%")).toBe("1rem");
   });
 });
